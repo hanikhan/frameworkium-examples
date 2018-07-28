a7eba8e4e461e87586582e11c6ba0756dac57213
Graham Russell <graham@ham1.co.uk>
2018-07-28 22:35:43 +0100

    Attempt to get travis working, remove Capture tests, update pom.

--- src/test/java/ai/capture/api/dto/executions/ExecutionID.java
+++
@@ -1,9 +0,0 @@
-package ai.capture.api.dto.executions;
-
-import com.frameworkium.api.dto.AbstractDTO;
-
-/** Created execution message. */
-public class ExecutionID extends AbstractDTO<ExecutionID> {
-
-    public String executionID;
-}
